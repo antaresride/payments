8cc2b69ef309eb0a1b8ff0cbccdf729cd3d094ad
antaresride <gledsoul@gmail.com>
2026-03-13 15:08:25 -0300

    Update payment.go

--- payment.go
+++ payment.go
@@ -1,31 +1,7 @@
-package main
-
-import (
-	"fmt"
-
-	"rsc.io/quote"
-)
-
-type Payment struct {
-	who string
-}
-type Provider interface {
-	blockchain() string
-}
-
-func Hello() string {
-	return "Hello!"
-}
-func (Payment) Provider() string {
-	return "Genpay!"
-}
-
 func main() {
-	var a = 1
-	a = 2
-	a = 3
-	b := 4
-	b = 5
+	// 1. Fixed "ineffectual assignment" by using the values or removing them
+	a := 3 
+	b := 5
 
 	var amount [10]float64
 	amount[0] = 12.00
@@ -44,15 +20,15 @@ func main() {
 	fmt.Println(payment1)
 	fmt.Println(payment2)
 
-	fmt.Println(payment1 == payment1)
+	// 2. Fixed "identical expressions" (don't compare a variable to itself)
+	fmt.Println(payment1 == payment2) 
 
 	fmt.Println(Hello())
-
 	fmt.Println(payment1.Provider())
-
 	fmt.Println(payment2.Provider())
 
 	fmt.Println(amount[0])
 
-	fmt.Println(quote.Go)
+	// 3. Fixed "func value not called": quote.Go is a function, call it with ()
+	fmt.Println(quote.Go()) 
 }
